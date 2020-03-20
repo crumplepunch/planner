a679c8e98d66052d6610b13d07bcd5d7703a7db7
Gabe Marquez <gabemarquez@protonmail.com>
2020-03-20 15:53:22 -0400

    planner frontend initial commit

---
+++ src/Planner/index.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+if (process.env.NODE_ENV !== 'production') {
+  console.log(process.env)
+}
+
+export default function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+      </header>
+    </div>
+  )
+}
--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import Planner from './Planner';
+import * as serviceWorker from './serviceWorker'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import './index.css'
+
+
+ReactDOM.render(<Planner />, document.getElementById('planner'))
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+serviceWorker.unregister()
