558a1b82a01fbc482c4771940bccf51940203e86
Gabe Marquez <gabemarquez@protonmail.com>
2020-03-23 21:05:15 -0400

    format hoc

--- src/components/hoc.js
+++ src/components/hoc.js
@@ -1,3 +1,4 @@
 import React from 'react'
 import { ApolloProvider } from '@apollo/react-hooks'
-export const useApollo = (client, Component) => props => <ApolloProvider client={client}><Component {...props}></Component>></ApolloProvider>
+
+export const useApollo = (client, Component) => props => <ApolloProvider client={client}><Component {...props}></Component></ApolloProvider>
