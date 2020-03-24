28800718f64b86f615f6884a029263e3767f83c0
Gabe Marquez <gabemarquez@protonmail.com>
2020-03-24 12:32:26 -0400

    Project component

--- src/Projects/index.js
+++ src/Projects/index.js
@@ -2,10 +2,17 @@ import React from 'react'
 import { useApollo } from '../components/hoc'
 import { useQuery } from '@apollo/react-hooks'
 import { client } from './apollo/index'
-
 import { GET_PROJECTS } from './apollo/queries'
 import debug from 'debug'
+
+
 const log = debug('Projects:log')
+const Project = ({ name, description }) => {
+  return <div className='project'>
+    <h1>{name}</h1>
+    <h3>{description}</h3>
+  </div>
+}
 
 const Projects = props => {
   const { data, loading, error } = useQuery(GET_PROJECTS)
@@ -19,8 +26,11 @@ const Projects = props => {
   if (error) return <h1> error</h1>
   if (!data) return <h1> 404 Not found</h1>
 
+  const { projects } = data
+
   return <div>
-    <h1> Found</h1>
+    <h1>Projects:</h1>
+    {projects.map((project, key) => <Project key={key} {...project}></Project>)}
   </div>
 }
 export default useApollo(client, Projects)
