98ba2cbdafa49d7e77c613fb915d6351a4344de0
Gabe Marquez <gabemarquez@protonmail.com>
2020-03-26 13:31:46 -0400

    cleanup

--- src/Projects/index.js
+++ src/Projects/index.js
@@ -12,23 +12,14 @@ import Project from './Project'
 const log = debug('Projects:general')
 const statelog = debug('Projects:state')
 
-const Action = ({ isFocused, focus, unfocus, label, onClick = e => { e.preventDefault() } }) => {
-  const props = {
-    onMouseEnter: e => {
-      e.preventDefault()
-      focus(e)
-    },
-    onMouseLeave: e => {
-      e.preventDefault()
-      unfocus(e)
-    },
-    onClick,
-    style: {
-      color: isFocused ? '#fff' : 'inherit'
-    }
-  }
-  return <div className='button' type='button' {...props}>{label}</div>
-}
+const Action = ({ isFocused, focus, unfocus, label, onClick = e => { e.preventDefault() } }) => <div
+  className='button'
+  type='button'
+  onMouseEnter={focus}
+  onMouseLeave={unfocus}
+  onClick={onClick}
+  style={{ color: isFocused ? '#fff' : 'inherit' }}
+>{label}</div>
 
 const Projects = props => {
   const { data, loading, error } = useQuery(GET_PROJECTS)
@@ -36,93 +27,73 @@ const Projects = props => {
   const [hoveredId, setHover] = useState(null)
   const [action, setAction] = useState('')
 
-  log({
-    data,
-    loading,
-    error
-  })
+  log({ data, loading, error })
 
-
-  const hoverPrev = _ => {
-    const index = projects.map(project => project._id).indexOf(hoveredId)
-    console.log({ index })
-    if (index <= 0) return
-    setHover(projects[index - 1]._id)
-  }
-  const hoverNext = _ => {
-    const index = projects.map(project => project._id).indexOf(hoveredId)
-    if (!(index < (projects.length - 1))) return
-    setHover(projects[index + 1]._id)
-  }
-
-  const vimKeyDown = ({ key }) => {
-    if (key === 'j') return hoverNext()
-    if (key === 'k') return hoverPrev()
-    if (key === 'Enter') return document.getElementById(hoveredId).classList.add('active')
-  }
-
-  const vimKeyUp = ({ key }) => {
-    if (key === 'Enter') {
-      document.getElementById(hoveredId).classList.remove('active')
-      setFocus(hoveredId)
-    }
-  }
+  const { projects } = data || []
 
   useEffect(() => {
+    const hoverPrev = _ => {
+      const index = projects.map(project => project._id).indexOf(hoveredId)
+      if (index <= 0) return
+      setHover(projects[index - 1]._id)
+    }
+    const hoverNext = _ => {
+      const index = projects.map(project => project._id).indexOf(hoveredId)
+      if (!(index < (projects.length - 1))) return
+      setHover(projects[index + 1]._id)
+    }
+    const vimKeyDown = ({ key }) => {
+      if (key === 'j') return hoverNext()
+      if (key === 'k') return hoverPrev()
+      if (key === 'Enter') return document.getElementById(hoveredId).classList.add('active')
+    }
+    const vimKeyUp = ({ key }) => {
+      if (key === 'Enter') {
+        document.getElementById(hoveredId).classList.remove('active')
+        setFocus(hoveredId)
+      }
+    }
+
     window.addEventListener('keydown', vimKeyDown)
     window.addEventListener('keyup', vimKeyUp)
     // Remove event listeners on cleanup
     return () => {
       window.removeEventListener('keydown', vimKeyDown)
       window.removeEventListener('keyup', vimKeyUp)
-
     }
-  }, [vimKeyDown, vimKeyUp])
+  }, [hoveredId, projects])
 
-  const actionFn = action => {
-    return e => {
-      setAction(action)
-    }
-  }
 
-  const setNone = actionFn('')
+
 
   if (loading) return <h1>Loading</h1>
   if (error) return <h1> error</h1>
   if (!data) return <h1> 404 Not found</h1>
+  if (!focusedId) setFocus(projects[0]._id)
+  if (!hoveredId) setHover(projects[0]._id)
 
-  const { projects } = data
-
-  if (!focusedId) {
-    setFocus(projects[0]._id)
-  }
-  if (!hoveredId) {
-    setHover(projects[0]._id)
-  }
-
-  const previousProject = _ => {
-    const index = projects.map(project => project._id).indexOf(focusedId)
-    if (index <= 0) return
-    setFocus(projects[index - 1]._id)
-  }
-  const nextProject = _ => {
-    const index = projects.map(project => project._id).indexOf(focusedId)
-    if (!(index < (projects.length - 1))) return
-    setFocus(projects[index + 1]._id)
+  const actions = {
+    prev: _ => {
+      const index = projects.map(project => project._id).indexOf(focusedId)
+      if (index <= 0) return
+      setFocus(projects[index + 1]._id)
+    },
+    next: _ => {
+      const index = projects.map(project => project._id).indexOf(focusedId)
+      if (!(index < (projects.length - 1))) return
+      setFocus(projects[index + 1]._id)
+    }
   }
 
 
+  const setNone = _ => setAction('')
 
   return <div className='projects container flex-column flex-grow' onClick={e => {
     statelog({
       currentProject: projects[projects.map(({ _id }) => _id).indexOf(focusedId)],
       currentAction: action
     })
-    if (action === 'next') {
-      nextProject(e)
-    } else if (action === 'prev') {
-      previousProject(e)
-    }
+    action && actions[action]()
   }}>
     <h1 className='max-width justify-center'>Projects</h1>
     <div className='flex-column max-flex-room'>
@@ -146,15 +117,16 @@ const Projects = props => {
     {
       <div className="max-width container justify-space-between footer">
         <div className="container">
-          <Action label='View' focus={actionFn('view')} unfocus={setNone} isFocused={action === 'view'}></Action>
-          <Action label='Edit' focus={actionFn('edit')} unfocus={setNone} isFocused={action === 'edit'}></Action>
+          <Action label='View' focus={_ => setAction('view')} unfocus={setNone} isFocused={action === 'view'}></Action>
+          <Action label='Edit' focus={_ => setAction('edit')} unfocus={setNone} isFocused={action === 'edit'}></Action>
         </div>
         <div className="container">
-          <Action label='▲' focus={actionFn('prev')} unfocus={setNone} isFocused={action === 'prev'} />
-          <Action label='▼' focus={actionFn('next')} unfocus={setNone} isFocused={action === 'next'} />
+          <Action label='▲' focus={_ => setAction('prev')} unfocus={setNone} isFocused={action === 'prev'} />
+          <Action label='▼' focus={_ => setAction('next')} unfocus={setNone} isFocused={action === 'next'} />
         </div>
       </div>
     }
   </div >
 }
+
 export default useApollo(client, Projects)
